293df2b263c43004360016b9e63e2efe5d46e248
Pedro Silveira Thiago <89752565+SCO-TClor@users.noreply.github.com>
2025-10-16 16:00:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function abrirCalcBoba() {
+    document.open('./CalcBoba.html','Calculadora Boba', 'width=425,height=500')
+}
+function abrirInsta() {
+    window.open('https://www.instagram.com/Pedro_by_Gwinfyd')
+}
